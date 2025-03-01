eaac3b84a5f4be963a29e499bfa9662c3c27b7cf
garyv <garyv@users.noreply.github.com>
2025-03-01 06:01:10 +0000

    deploy: 3ec8122d7071023c2818dcc33bbe9878659b71e5

---
+++ cache-worker.js
@@ -0,0 +1,65 @@
+
+const cacheName = 'client-cache-0-01';
+const cachePaths = ['/'];
+
+const cacheEvents = {
+    open: async () => {
+        return await caches.open(cacheName);
+    },
+
+    add: async () => {
+        const cache = await cacheEvents.open();
+        cachePaths.forEach ( async (path) => {
+            await cache.add(path);
+        })
+    },
+    fetch: async (request) => {
+        const cache = await cacheEvents.open();
+        const cachedResponse = await cache.match(request);
+        return cachedResponse || fetch(request);
+    },
+
+    update: async () => {
+        const cache = await cacheEvents.open();
+        cachePaths.forEach( async (path) => {
+            const cachedResponse = await cache.match(path);
+            if (!cachedResponse) return;
+            const etag = cachedResponse.headers.get('etag');
+            const response = await fetch(path, { method: 'HEAD' });
+            console.log('cache update in progress', { path, etag, response, cache });
+            if (!response.ok) return;
+            if (response.headers.get('etag') === etag) return;
+            await cache.delete(path);
+            await cache.add(path);
+            console.log('cache updated', { path, etag });
+        })
+    }
+};
+
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        cacheEvents
+          .add()
+          .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        cacheEvents.fetch(event.request)
+    );
+
+    if (event.request.url.endsWith('/')) {
+        event.waitUntil(
+            cacheEvents.update()
+        )
+    }
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        self.clients.claim()
+    )
+});
+
